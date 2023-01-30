1c12e2c70eb2407c22717daa727e7e9b7b1cf175
Kilian <sadq@web.de>
2023-01-30 21:57:25 +0100

    Wireframe

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use std::{thread, time::Duration};
+
+//use log::{debug, error, info, warn};
+
 // Execute main function
 fn main() {
-    println!("Hello, world!");
+
+    // Setup config
+    let attributes_path = "C:/Program Files (x86)/Steam/steamapps/common/Hunt Showdown/user/profiles/default/attributes.xml";
+    let mut n = 1;
+
+    // TODO: Start loop
+    while n <= 10 {
+        println!("Attributes Path: {:?}", attributes_path);
+
+        thread::sleep(Duration::from_secs(5));
+        // TODO: Read attributes xml
+
+        // TODO: Read steam file
+
+        // TODO: Produce kafka message
+
+        n += 1;
+    }
 }
