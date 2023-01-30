ebcc52472f59cd966977edf8c6aa874a4183e252
Kilian <sadq@web.de>
2023-01-30 22:15:45 +0100

    baseline logging

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use std::{thread, time::Duration};
 
-//use log::{debug, error, info, warn};
+use log::{error, warn, info, debug};
+use env_logger::Env;
+
 
 // Execute main function
 fn main() {
+    // SETUP LOGGER
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     // Setup config
     let attributes_path = "C:/Program Files (x86)/Steam/steamapps/common/Hunt Showdown/user/profiles/default/attributes.xml";
-    let mut n = 1;
-
+    info!(target: "Configuration", "Attributes Path: {:?}", attributes_path);
+    
+    
     // TODO: Start loop
+    let mut n = 1;
     while n <= 10 {
-        println!("Attributes Path: {:?}", attributes_path);
-
+        info!(target: "Monitoring", "Checking file: {:?}", attributes_path);
         thread::sleep(Duration::from_secs(5));
         // TODO: Read attributes xml
 
