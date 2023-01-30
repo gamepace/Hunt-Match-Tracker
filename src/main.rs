9128aa8c0bd99df91f6be902cd8e532436b96005
Kilian <sadq@web.de>
2023-01-30 22:26:23 +0100

    Read attributes.xml

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,16 @@ fn main() {
     let attributes_path = "C:/Program Files (x86)/Steam/steamapps/common/Hunt Showdown/user/profiles/default/attributes.xml";
     info!(target: "Configuration", "Attributes Path: {:?}", attributes_path);
     
-    
-    // TODO: Start loop
+    // Start loop
     let mut n = 1;
     while n <= 10 {
         info!(target: "Monitoring", "Checking file: {:?}", attributes_path);
         thread::sleep(Duration::from_secs(5));
-        // TODO: Read attributes xml
+        // TODO: #2 Read attributes xml @kggx
 
-        // TODO: Read steam file
+        // TODO: #3 Read steam file @kggx
 
-        // TODO: Produce kafka message
+        // TODO: #4 Produce kafka message @kggx
 
         n += 1;
     }
