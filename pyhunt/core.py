6cac5daf89fb6e0101731807dcd214bd99cf7b83
Kilian <sadq@web.de>
2023-02-16 17:05:35 +0100

    Monitor attributes.xml @kggx Fixes #8

--- pyhunt/core.py
+++ pyhunt/core.py
@@ -1,24 +1,61 @@
 from .helper import *
+import datetime
+import time
+import keyboard
+import logging
 
+# Debug
+import json
 
-####################################
-# Logging Setup
+######################################################################################################
 
-
-####################################
 class huntClient():
-    def __init__(self) -> None:
+    def __init__(self, log_level:str='INFO') -> None:
+        # Initialize Logger
+        self.logger = logging.Logger('Hunt Match Tracker')
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
         
-        steam = steamHelper()
-        attributes_path = steam.get_hunt_attributes()
-        print(attributes_path)
+        level = logging.getLevelName(log_level.upper())
+        self.logger.setLevel(level)
+        self.logger.info('Initialized Logger...')
         
-        print('Hello World')
-        input("Press any key to quit...")
+        # Initialize Client
+        self.logger.info('Initialize Client...')
+        self.steam = steamHelper()
+        self.hunt = huntHelper()
+        
+        self.logger.info('Get Hunt: Showdow attributes.xml path...')
+        self.attributes_path = self.steam.get_hunt_attributes()
+                    
         pass
         
     def monitor(self):
-        # TODO: #8 Monitor attributes.xml @kggx
+        # Monitor attributes.xml 
+        # TODO: #11 Implement a better quitting process @kggx
+        while True:
+            self.logger.info('Monitor attributes.xml (Hold ESC to quit!) ...')
+            if keyboard.is_pressed('esc'):
+                # TODO: #9 Quit routine @kggx
+                break
+            
+            # Read attributes
+            self.logger.info('Parse Hunt: Showdown attributes.xml to json ...')
+            self.json_attributes = self.hunt.get_hunt_json_attributes(self.attributes_path)       
+            
+            # If loggin level is debug then export
+            if self.logger.getEffectiveLevel() == 10:
+                # TODO: #10 Fix issue with debug location @kggx
+                temp_attributes = Path("./tmp/attributes.json")
+                self.logger.debug(f'Write json to {temp_attributes.absolute()} ...')
+                with open(temp_attributes, 'w') as f:
+                    json.dump(self.json_attributes, f, indent=1)
+                    
+            time.sleep(5)
+        
+        
         pass
 
     def exit_procedure(self):
