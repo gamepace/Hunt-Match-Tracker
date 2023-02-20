a65ad4e9440a9e1ed8d8d20b3f20a025406cddcc
Kilian <sadq@web.de>
2023-02-20 20:00:19 +0100

    Implement a exit procedure that saves the latest information. @kggx Fixes #7

--- pyhunt/core.py
+++ pyhunt/core.py
@@ -34,36 +34,37 @@ class huntClient():
         
     def monitor(self):
         # Monitor attributes.xml 
-        # TODO: #11 Implement a better quitting process @kggx
-        while True:
-            self.logger.info('Monitor attributes.xml (Hold ESC to quit!) ...')
-            if keyboard.is_pressed('esc'):
-                # TODO: #9 Quit routine @kggx
-                break
-            
-            # Read attributes
-            self.logger.info('Parse Hunt: Showdown attributes.xml to json ...')
-            self.json_attributes = self.hunt.get_hunt_json_attributes(self.attributes_path)       
-            
-            # If logging level is debug then export
-            if self.logger.getEffectiveLevel() == 10:
-                # TODO: #10 Fix issue with debug location @kggx
-                temp_attributes = Path("./tmp/attributes.json")
-                self.logger.debug(f'Write json to {temp_attributes.absolute()} ...')
-                with open(temp_attributes, 'w') as f:
-                    json.dump(self.json_attributes, f, indent=1)
-                    
-            time.sleep(5)
+        try:
+            while True:
+                
+                self.logger.info('Monitor attributes.xml (Press CTRL+C to quit!) ...')
+                
+                # Read attributes
+                self.logger.info('Parse Hunt: Showdown attributes.xml to json ...')
+                self.json_attributes = self.hunt.get_hunt_json_attributes(self.attributes_path)       
+                
+                # If logging level is debug then export
+                if self.logger.getEffectiveLevel() == 10:
+                    # TODO: #10 Fix issue with debug location @kggx
+                    temp_attributes = Path("./tmp/attributes.json")
+                    self.logger.debug(f'Write json to {temp_attributes.absolute()} ...')
+                    with open(temp_attributes, 'w') as f:
+                        json.dump(self.json_attributes, f, indent=1)
+                        
+                
 
-            # TODO: #12 Transform json attributes @kggx
+                # TODO: #12 Transform json attributes @kggx
+                
+                # TODO: #13 Implement match meta parsing @kggx
+                
+                # TODO: #5 Implement player results @kggx
             
-            # TODO: #13 Implement match meta parsing @kggx
+                time.sleep(5)
             
-            # TODO: #5 Implement player results @kggx
-            
-            
-        pass
-
+        except KeyboardInterrupt:
+            # TODO: #9 Quit routine @kggx
+            pass
+      
     def exit_procedure(self):
         # TODO: #7 Implement a exit procedure that saves the latest information. @kggx
         pass
